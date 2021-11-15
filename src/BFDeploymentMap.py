54a1012b799f8e1208f464a8d2d78ec21d20baf6
Michael Schwarz <mschwarz@multitool.net>
2021-11-15 09:38:10 -0600

    Apologies. Broken commit. Need to transfer. Also need to look at better gitignore specs, but this is okay for now.

--- src/BFDeploymentMap.py
+++ src/BFDeploymentMap.py
@@ -2,12 +2,16 @@ import bigfixREST
 import argparse
 import ipaddress
 import graphviz
+import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--bfserver", type=str, help="BigFix REST Server name/IP address", required=True)
+parser.add_argument("-s", "--bfserver", type=str, help="BigFix REST Server name/IP address")
 parser.add_argument("-p", "--bfport", type=int, help="BigFix Port number (default 52311)", default=52311)
-parser.add_argument("-U", "--bfuser", type=str, help="BigFix Console/REST User name", required=True)
-parser.add_argument("-P", "--bfpass", type=str, help="BigFix Console/REST Password", required=True)
+parser.add_argument("-U", "--bfuser", type=str, help="BigFix Console/REST User name")
+parser.add_argument("-P", "--bfpass", type=str, help="BigFix Console/REST Password")
+parser.add_argument("-w", "--writejson", type=str, help="Write query results to json file for reuse.")
+parser.add_argument("-j", "--json", type=str, help="Use JSON from previous run instead of doing REST query")
+parser.add_argument("-o", "--output", type=str, help="Output file base name", default="DeploymentMap")
 parser.add_argument("-g", "--groupProperty", type=str, 
     help="Name of BigFix Computer Property to group/count on", default="Subnet Address")
 parser.add_argument('-m', "--map", type=str, 
@@ -15,129 +19,146 @@ parser.add_argument('-m', "--map", type=str,
 parser.add_argument("-d", "--detail", action='store_true', help="Create nodes for each endpoint")
 conf = parser.parse_args()
 
-# First, pull all the "registration servers"
-# We have to query separately because we need the root and relays in place first
-# so we can assign regular endpoints to them as we process them. So two queries.
-treeme = f'''(id of it, name of it as lowercase, 
-last report time of it, relay server flag of it, 
-root server flag of it, relay server of it as lowercase,
-concatenation "|" of (ip addresses of it as string),
-concatenation "|" of 
-values of property results whose (name of property of it = "{conf.groupProperty}") of it
-) 
-of bes computers whose (relay server flag of it or root server flag of it)
-'''.strip()
 
-# Now pull all the "regular" endpoints
-compme = f'''(id of it, name of it as lowercase, 
-last report time of it, relay server flag of it, 
-root server flag of it, relay server of it as lowercase,
-concatenation "|" of (ip addresses of it as string),
-concatenation "|" of 
-values of property results whose (name of property of it = "{conf.groupProperty}") of it
-) 
-of bes computers whose (not relay server flag of it and not root server flag of it)
-'''.strip()
+### The BIG switch: Are we reading a JSON file, or running a query?
 
-# Initialize the relay map
-rMap = {}
+if conf.json:
+    with open(conf.json, "r") as jsonfh:
+        jsdata = [].append(jsonfh.readlines())
+        jd = json.loads(jsdata.join(" "))
+        relay = jd["relay"] 
+else:
+    # First, pull all the "registration servers"
+    # We have to query separately because we need the root and relays in place first
+    # so we can assign regular endpoints to them as we process them. So two queries.
+    treeme = f'''(id of it, name of it as lowercase, 
+    last report time of it, relay server flag of it, 
+    root server flag of it, relay server of it as lowercase,
+    concatenation "|" of (ip addresses of it as string),
+    concatenation "|" of 
+    values of property results whose (name of property of it = "{conf.groupProperty}") of it
+    ) 
+    of bes computers whose (relay server flag of it or root server flag of it)
+    '''.strip()
 
-if conf.map:
-    for mapval in str(conf.map).split(","):
-        name, value = mapval.split(":")
-        rMap[name] = value
+    # Now pull all the "regular" endpoints
+    compme = f'''(id of it, name of it as lowercase, 
+    last report time of it, relay server flag of it, 
+    root server flag of it, relay server of it as lowercase,
+    concatenation "|" of (ip addresses of it as string),
+    concatenation "|" of 
+    values of property results whose (name of property of it = "{conf.groupProperty}") of it
+    ) 
+    of bes computers whose (not relay server flag of it and not root server flag of it)
+    '''.strip()
 
-bf = bigfixREST.bigfixRESTConnection(conf.bfserver, int(conf.bfport), conf.bfuser, conf.bfpass)
+    # Initialize the relay map
+    rMap = {}
 
-rqr = bf.srQueryJson(treeme)
-cqr = bf.srQueryJson(compme)
+    if conf.map:
+        for mapval in str(conf.map).split(","):
+            name, value = mapval.split(":")
+            rMap[name] = value
 
-compList = rqr['result'] + cqr['result']
+    bf = bigfixREST.bigfixRESTConnection(conf.bfserver, int(conf.bfport), conf.bfuser, conf.bfpass)
 
-print(cqr)
+    rqr = bf.srQueryJson(treeme)
+    cqr = bf.srQueryJson(compme)
 
-# This will hold a dictionary of relay names
-relay = {}
-# This will hold a dictionary of IP Addresses that refer to the same 
-# relay "objects" as the relay dict. We call this "ipIdx", but it will use
-# unique values of whatever BigFix computer property you use as the -g 
-# command line switch.
-ipIdx = {}
+    compList = rqr['result'] + cqr['result']
 
-root = None
+    print(cqr)
 
-for comp in compList:
-    if comp[4] == True:
-        # This is the root server
-        print("*****ROOT*****")
-        root = comp[1]
-        relay[root] = {}
-        relay[root]['comp'] = comp
-        relay[root]['count'] = 1
-        relay[root]['groups'] = {}
-        # Root is its own parent (I decided)
-        relay[root]['parent'] = comp[1]
-        for ip in str(comp[6]).split("|"):
-            ipIdx[ip] = relay[root]
-    elif comp[3] == True:
-        # This is a relay
-        print("------------> RELAY")
-        rhost = comp[1]
-        relay[rhost] = {}
-        relay[rhost]['comp'] = comp
-        relay[rhost]['count'] = 1
-        relay[rhost]['groups'] = {}
-        relay[rhost]['parent'] = str(comp[5]).split(f":{conf.bfport}")[0]
-        for ip in str(comp[6]).split("|"):
-            ipIdx[ip] = relay[rhost]
-    else:
-        # This is an endpoint
-        print("endpoint <-------------")
-        print(comp)
-        # First, find the endpoint's relay
-        rName = str(comp[5]).split(f":{conf.bfport}")[0]
+    # This will hold a dictionary of relay names
+    relay = {}
+    # This will hold a dictionary of IP Addresses that refer to the same 
+    # relay "objects" as the relay dict. We call this "ipIdx", but it will use
+    # unique values of whatever BigFix computer property you use as the -g 
+    # command line switch.
+    ipIdx = {}
 
-        # See if we have an ip address on our hands
-        try:
-            ip = ipaddress.ip_address(rName)
-        except ValueError:
-            ip = None
+    root = None
 
-        # if we do not, see if we have domain suffix on the host name
-        if not ip:
-            if rName.find(".") > 0:
-                rName = rName[0:rName.find(".")]
-
-        epRelay = None
-
-        if rName in relay.keys():
-            # We can find the relay by name key
-            epRelay = relay[rName]
+    for comp in compList:
+        if comp[4] == True:
+            # This is the root server
+            print("*****ROOT*****")
+            root = comp[1]
+            relay[root] = {}
+            relay[root]['comp'] = comp
+            relay[root]['count'] = 1
+            relay[root]['groups'] = {}
+            # Root is its own parent (I decided)
+            relay[root]['parent'] = comp[1]
+            for ip in str(comp[6]).split("|"):
+                ipIdx[ip] = relay[root]
+        elif comp[3] == True:
+            # This is a relay
+            print("------------> RELAY")
+            rhost = comp[1]
+            relay[rhost] = {}
+            relay[rhost]['comp'] = comp
+            relay[rhost]['count'] = 1
+            relay[rhost]['groups'] = {}
+            relay[rhost]['parent'] = str(comp[5]).split(f":{conf.bfport}")[0]
+            for ip in str(comp[6]).split("|"):
+                ipIdx[ip] = relay[rhost]
         else:
-            # We need to try ip addresses
-            if rName in ipIdx.keys():
-                epRelay = ipIdx[rName]
-            elif rName in rMap.keys():
-                epRelay = relay[rMap[rName]]
+            # This is an endpoint
+            print("endpoint <-------------")
+            print(comp)
+            # First, find the endpoint's relay
+            rName = str(comp[5]).split(f":{conf.bfport}")[0]
+
+            # See if we have an ip address on our hands
+            try:
+                ip = ipaddress.ip_address(rName)
+            except ValueError:
+                ip = None
+
+            # if we do not, see if we have domain suffix on the host name
+            if not ip:
+                if rName.find(".") > 0:
+                    rName = rName[0:rName.find(".")]
+
+            epRelay = None
+
+            if rName in relay.keys():
+                # We can find the relay by name key
+                epRelay = relay[rName]
             else:
-                print("Relay not found")
+                # We need to try ip addresses
+                if rName in ipIdx.keys():
+                    epRelay = ipIdx[rName]
+                elif rName in rMap.keys():
+                    epRelay = relay[rMap[rName]]
+                else:
+                    print("Relay not found")
 
-        # If we could not find the relay, we must skip the endpoint
-        if (epRelay == None):
-            print(f"Warning: We could not locate the relay {rName} using name or IP address")
-            continue
+            # If we could not find the relay, we must skip the endpoint
+            if (epRelay == None):
+                print(f"Warning: We could not locate the relay {rName} using name or IP address")
+                continue
 
-        # Increment the unique count
-        epRelay['count'] += 1
+            # Increment the unique count
+            epRelay['count'] += 1
 
-        for grp in str(comp[7]).split("|"):
-            if grp in epRelay['groups'].keys():
-                epRelay['groups'][grp]['count'] += 1
-                epRelay['groups'][grp]['compList'] += [comp]
-            else:
-                epRelay['groups'][grp] = {'count' : 1,
-                    'compList' : [comp] }
-        
+            for grp in str(comp[7]).split("|"):
+                if grp in epRelay['groups'].keys():
+                    epRelay['groups'][grp]['count'] += 1
+                    epRelay['groups'][grp]['compList'] += [comp]
+                else:
+                    epRelay['groups'][grp] = {'count' : 1,
+                        'compList' : [comp] }
+
+    if conf.writejson:
+        jsdata = {}
+        jsdata["relay"] = relay
+
+        with open(conf.writejson, "w") as jsonfh:
+            jsonfh.write(json.dumps(jsdata))
+
+
 ## AT THIS POINT we have a recursive data structure containing all the relays in
 ## the deployment with all the endpoints attached to them, grouped by the grouping
 ## property. Let's start rendering with graphviz
