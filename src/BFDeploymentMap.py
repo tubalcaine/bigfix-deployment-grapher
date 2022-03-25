f67887cfd98543522002faf0797bc588db025d74
Michael Schwarz <michael/schwarz@hcl.com>
2022-03-25 14:23:35 -0500

    Some pylint cleanup.

--- src/BFDeploymentMap.py
+++ src/BFDeploymentMap.py
@@ -1,3 +1,6 @@
+"""
+BFDeploymentMap.py - A script to graph BigFix deployment using GraphViz
+"""
 import argparse
 import ipaddress
 import json
@@ -23,9 +26,10 @@ import bigfixREST
 ##
 ## https://graphviz.org/download/
 ##
-## NB: This script (like many do) grew well beyond its original intent. For example, the -w/-j options
-## to allow query results to be stored and re-used was added quite late, and its implementation, while
-## okay, consisted of throwing a big if-else around an already overlong script.
+## NB: This script (like many do) grew well beyond its original intent. For example, the -w/-j
+## options to allow query results to be stored and re-used was added quite late, and its
+## implementation, while okay, consisted of throwing a big if-else around an already overlong
+## script.
 ##
 ## There are redundant identifiers in abundance. And as an old c/c++/Java prgorammer, I do
 ## not like undeclared identifiers or identifiers of module or global scope. So there
@@ -33,25 +37,52 @@ import bigfixREST
 ## so I'm putting it out there. Contact me through GitHub if you'd like to help!
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--bfserver", type=str, help="BigFix REST Server name/IP address")
-parser.add_argument("-p", "--bfport", type=int, help="BigFix Port number (default 52311)", default=52311)
+parser.add_argument(
+    "-s", "--bfserver", type=str, help="BigFix REST Server name/IP address"
+)
+parser.add_argument(
+    "-p", "--bfport", type=int, help="BigFix Port number (default 52311)", default=52311
+)
 parser.add_argument("-U", "--bfuser", type=str, help="BigFix Console/REST User name")
 parser.add_argument("-P", "--bfpass", type=str, help="BigFix Console/REST Password")
-parser.add_argument("-w", "--writejson", type=str, help="Write query results to json file for reuse.")
-parser.add_argument("-j", "--json", type=str, help="Use JSON from previous run instead of doing REST query")
-parser.add_argument("-o", "--output", type=str,
-    help="Output file base name", default="DeploymentMap")
-parser.add_argument("-e", "--engine", type=str,
+parser.add_argument(
+    "-w", "--writejson", type=str, help="Write query results to json file for reuse."
+)
+parser.add_argument(
+    "-j",
+    "--json",
+    type=str,
+    help="Use JSON from previous run instead of doing REST query",
+)
+parser.add_argument(
+    "-o", "--output", type=str, help="Output file base name", default="DeploymentMap"
+)
+parser.add_argument(
+    "-e",
+    "--engine",
+    type=str,
     help="Specify the graphviz layout engine (dot, neato, etc.)",
-    default="dot")
-parser.add_argument("-f", "--format", type=str, help="Specify the output format",
-    default="pdf")
-parser.add_argument("-g", "--groupProperty", type=str,
-    help="Name of BigFix Computer Property to group/count on", default="Subnet Address")
-parser.add_argument('-m', "--map", type=str,
-    help="Relay name map fromName:toName[,fromName:toName...]")
-parser.add_argument("-r", "--relaysonly", action="store_true", help="Render relays only")
-parser.add_argument("-d", "--detail", action='store_true', help="Create nodes for each endpoint")
+    default="dot",
+)
+parser.add_argument(
+    "-f", "--format", type=str, help="Specify the output format", default="pdf"
+)
+parser.add_argument(
+    "-g",
+    "--groupProperty",
+    type=str,
+    help="Name of BigFix Computer Property to group/count on",
+    default="Subnet Address",
+)
+parser.add_argument(
+    "-m", "--map", type=str, help="Relay name map fromName:toName[,fromName:toName...]"
+)
+parser.add_argument(
+    "-r", "--relaysonly", action="store_true", help="Render relays only"
+)
+parser.add_argument(
+    "-d", "--detail", action="store_true", help="Create nodes for each endpoint"
+)
 conf = parser.parse_args()
 
 if not conf.json and (not conf.bfserver or not conf.bfuser or not conf.bfpass):
@@ -76,7 +107,7 @@ else:
     # First, pull all the "registration servers"
     # We have to query separately because we need the root and relays in place first
     # so we can assign regular endpoints to them as we process them. So two queries.
-    treeme = f'''(id of it, name of it as lowercase,
+    treeme = f"""(id of it, name of it as lowercase,
     last report time of it, relay server flag of it, 
     root server flag of it, relay server of it as lowercase,
     concatenation "|" of (ip addresses of it as string),
@@ -84,10 +115,10 @@ else:
     values of property results whose (name of property of it = "{conf.groupProperty}") of it
     ) 
     of bes computers whose (relay server flag of it or root server flag of it)
-    '''.strip()
+    """.strip()
 
     # Now pull all the "regular" endpoints
-    compme = f'''(id of it, name of it as lowercase,
+    compme = f"""(id of it, name of it as lowercase,
     last report time of it, relay server flag of it, 
     root server flag of it, relay server of it as lowercase,
     concatenation "|" of (ip addresses of it as string),
@@ -95,10 +126,10 @@ else:
     values of property results whose (name of property of it = "{conf.groupProperty}") of it
     ) 
     of bes computers whose (not relay server flag of it and not root server flag of it)
-    '''.strip()
+    """.strip()
 
     # Initialize the relay map
-    rMap = { }
+    rMap = {}
 
     if conf.map:
         for mapval in str(conf.map).split(","):
@@ -106,7 +137,9 @@ else:
             rMap[name] = value
 
     ## Must have EITHER --json or --bfserver
-    bf = bigfixREST.bigfixRESTConnection(conf.bfserver, int(conf.bfport), conf.bfuser, conf.bfpass)
+    bf = bigfixREST.bigfixRESTConnection(
+        conf.bfserver, int(conf.bfport), conf.bfuser, conf.bfpass
+    )
 
     rqr = bf.srQueryJson(treeme)
 
@@ -114,7 +147,7 @@ else:
     # of this app. Surprisingly, implementing it was a lot easier than
     # planned!
     cqr = bf.srQueryJson(compme)
-    compList = rqr['result'] + cqr['result']
+    compList = rqr["result"] + cqr["result"]
 
     # This will hold a dictionary of relay names
     relay = {}
@@ -130,51 +163,51 @@ else:
             print("*****ROOT*****")
             root = comp[1]
             relay[root] = {}
-            relay[root]['comp'] = comp
-            relay[root]['count'] = 1
-            relay[root]['groups'] = {}
+            relay[root]["comp"] = comp
+            relay[root]["count"] = 1
+            relay[root]["groups"] = {}
             # Root is its own parent (I decided)
-            relay[root]['parent'] = comp[1]
-            
-            for m in rMap:
-                if relay[root]['parent'] == m:
-                    relay[root]['parent'] = rMap[m]
+            relay[root]["parent"] = comp[1]
 
-            for ip in str(comp[6]).split("|"):
-                ipIdx[ip] = relay[root]
+            for m in rMap:
+                if relay[root]["parent"] == m:
+                    relay[root]["parent"] = rMap[m]
+
+            for IP in str(comp[6]).split("|"):
+                ipIdx[IP] = relay[root]
         elif comp[3] is True:
             # This is a relay
             print("------------> RELAY")
             rhost = comp[1]
             relay[rhost] = {}
-            relay[rhost]['comp'] = comp
-            relay[rhost]['count'] = 1
-            relay[rhost]['groups'] = {}
-            relay[rhost]['parent'] = str(comp[5]).split(f":{conf.bfport}", maxsplit=1)[0]
+            relay[rhost]["comp"] = comp
+            relay[rhost]["count"] = 1
+            relay[rhost]["groups"] = {}
+            relay[rhost]["parent"] = str(comp[5]).split(f":{conf.bfport}", maxsplit=1)[
+                0
+            ]
             for m in rMap:
-                if relay[rhost]['parent'] == m:
-                    relay[rhost]['parent'] = rMap[m]
-            for ip in str(comp[6]).split("|"):
-                ipIdx[ip] = relay[rhost]
+                if relay[rhost]["parent"] == m:
+                    relay[rhost]["parent"] = rMap[m]
+            for IP in str(comp[6]).split("|"):
+                ipIdx[IP] = relay[rhost]
         else:
             # This is an endpoint
             print("endpoint <-------------")
             print(comp)
             # First, find the endpoint's relay
-            rName = str(comp[5]).split(f":{conf.bfport}")[0]
+            rName = str(comp[5]).split(f":{conf.bfport}", maxsplit=1)[0]
 
             # See if we have an ip address on our hands
             try:
-                ip = ipaddress.ip_address(rName)
+                IP = ipaddress.ip_address(rName)
             except ValueError:
-                ip = None
+                IP = None
 
             # if we do not, see if we have domain suffix on the host name
-            if not ip:
+            if not IP:
                 if rName.find(".") > 0:
-                    rName = rName[0:rName.find(".")]
-
-            epRelay = None
+                    rName = rName[0 : rName.find(".")]
 
             if rName in relay.keys():
                 # We can find the relay by name key
@@ -186,23 +219,25 @@ else:
                 elif rName in rMap.keys():
                     epRelay = relay[rMap[rName]]
                 else:
+                    epRelay = None
                     print("Relay not found")
 
             # If we could not find the relay, we must skip the endpoint
             if epRelay is None:
-                print(f"Warning: We could not locate the relay {rName} using name or IP address")
+                print(
+                    f"Warning: We could not locate the relay {rName} using name or IP address"
+                )
                 continue
 
             # Increment the unique count
-            epRelay['count'] += 1
+            epRelay["count"] += 1
 
             for grp in str(comp[7]).split("|"):
-                if grp in epRelay['groups'].keys():
-                    epRelay['groups'][grp]['count'] += 1
-                    epRelay['groups'][grp]['compList'] += [comp]
+                if grp in epRelay["groups"].keys():
+                    epRelay["groups"][grp]["count"] += 1
+                    epRelay["groups"][grp]["compList"] += [comp]
                 else:
-                    epRelay['groups'][grp] = {'count' : 1,
-                        'compList' : [comp] }
+                    epRelay["groups"][grp] = {"count": 1, "compList": [comp]}
 
     if conf.writejson:
         jsdata = {}
@@ -218,31 +253,37 @@ else:
 ## property. Let's start rendering with graphviz
 
 dot = graphviz.Digraph(cnf["bfserver"] + ":" + str(cnf["bfport"]), engine=conf.engine)
-dot.attr(concentrate="true", fontsize="14",
-    ratio="auto",
-    rankdir="BT")
-dot.attr('node', fontsize="10.0", fontname="Arial")
+dot.attr(concentrate="true", fontsize="14", ratio="auto", rankdir="BT")
+dot.attr("node", fontsize="10.0", fontname="Arial")
 
 for r in relay.keys():
     rly = relay[r]
-    dot.node(r, color="red", shape="box3d",
+    dot.node(
+        r,
+        color="red",
+        shape="box3d",
         ## Add group parameters to the relay label.
-        root=str(rly["comp"][4]), label=f"{r} - {rly['count']} unique endpoints\n")
-    dot.edge(r, rly['parent'], penwidth="1.5")
+        root=str(rly["comp"][4]),
+        label=f"{r} - {rly['count']} unique endpoints\n",
+    )
+    dot.edge(r, rly["parent"], penwidth="1.5")
 
     ## Now we still query for all computers in "relaysonly" so we can get an accurate count
     if not conf.relaysonly:
-        for c in rly['groups'].keys():
-            grp = rly['groups'][c]
-    #        dot.node(c, color="green", label=f"{conf.groupProperty} {c} - {grp['count']} endpoints", shape="box")
-    #        dot.edge(c, r, penwidth="1.5")
+        for c in rly["groups"].keys():
+            grp = rly["groups"][c]
             ## Detail means a node for every endpoint
             if conf.detail:
-                for ep in grp['compList']:
+                for ep in grp["compList"]:
                     dot.node(ep[1], color="blue", shape="component")
                     dot.edge(ep[1], r, penwidth="1.5")
             else:
-                dot.node(c, color="green", label=f"{conf.groupProperty} {c} - {grp['count']} endpoints", shape="box")
+                dot.node(
+                    c,
+                    color="green",
+                    label=f"{conf.groupProperty} {c} - {grp['count']} endpoints",
+                    shape="box",
+                )
                 dot.edge(c, r, penwidth="1.5")
 
 dot.unflatten(stagger=3).render(conf.output, format=conf.format)
--- src/bigfixREST.py
+++ src/bigfixREST.py
@@ -4,13 +4,13 @@ import xml.etree.ElementTree as ET
 
 # This is here ONLY to suppress self-signed certoficate warnings
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # End of warning supression
 
 
 ## bigFixActionResult class
-class bigfixActionResult():
-
+class bigfixActionResult:
     def __init__(self, resxml):
         self.xml = resxml
         self.root = ET.fromstring(resxml)
@@ -20,52 +20,41 @@ class bigfixActionResult():
         id = thing[0].text
         return id
 
-
     def getActionURL(self):
         thing = self.root.findall("Action")
         attrs = thing[0].attrib
         return attrs["Resource"]
 
-
     def getActionResultXML(self):
         return self.xml
 
 
-
 ## bigfixRESTConnection class
-class bigfixRESTConnection():
-
+class bigfixRESTConnection:
     def __init__(self, bfserver, bfport, bfuser, bfpass):
         self.bfserver = bfserver
         self.bfport = bfport
         self.bfuser = bfuser
         self.bfpass = bfpass
         self.sess = requests.Session()
-        self.url = "https://"  + self.bfserver + ":" + str(self.bfport) 
+        self.url = "https://" + self.bfserver + ":" + str(self.bfport)
 
         self.sess.auth = (self.bfuser, self.bfpass)
         resp = self.sess.get(self.url + "/api/login", verify=False)
 
     def srQueryJson(self, srquery):
-        qheader = {
-            'Content-Type' : 'application/x-www-form-urlencoded'
-        }
+        qheader = {"Content-Type": "application/x-www-form-urlencoded"}
 
-        qquery = {
-            "relevance" : srquery,
-            "output"    : "json"
-        }
-        
-        req = requests.Request('POST'
-            , self.url + "/api/query"
-            , headers=qheader
-            , data=qquery
-            )
-            
+        qquery = {"relevance": srquery, "output": "json"}
+
+        req = requests.Request(
+            "POST", self.url + "/api/query", headers=qheader, data=qquery
+        )
+
         prepped = self.sess.prepare_request(req)
-        result = self.sess.send(prepped, verify = False)
-        
-        if (result.status_code == 200):
+        result = self.sess.send(prepped, verify=False)
+
+        if result.status_code == 200:
             rv = json.loads(result.text)
             rv["query"] = srquery
             return rv
@@ -77,11 +66,18 @@ class bigfixRESTConnection():
     # each row of which contains a "row" entry with a flat array and a "dict" entry with
     # the mangled names and values. Usually when you write a relevance query, you know what the
     # positions are in absolute terms. I haven't decided if this is a good idea...
-#    def flattenQueryResult(self, qres):
-#        return None
+    #    def flattenQueryResult(self, qres):
+    #        return None
 
-    def takeSourcedFixletAction(self, targetList, siteId, fixletId, actionId = "Action1", title = "Programmatic Action from Python Script"):
-        templ = '''\
+    def takeSourcedFixletAction(
+        self,
+        targetList,
+        siteId,
+        fixletId,
+        actionId="Action1",
+        title="Programmatic Action from Python Script",
+    ):
+        templ = """\
 <?xml version="1.0" encoding="UTF-8" ?>
 <BES xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" >
 <SourcedFixletAction>
@@ -98,7 +94,7 @@ class bigfixRESTConnection():
 	<Title>__Title__</Title>
 </SourcedFixletAction>
 </BES>
-'''.strip()
+""".strip()
 
         templ = templ.replace("__SiteID__", str(siteId))
         templ = templ.replace("__FixletID__", str(fixletId))
@@ -110,24 +106,19 @@ class bigfixRESTConnection():
             targets += "<ComputerName>" + tgt + "</ComputerName>\n"
 
         templ = templ.replace("__TargetList__", targets)
-        
-        qheader = {
-            'Content-Type' : 'application/x-www-form-urlencoded'
-        }
 
-        req = requests.Request('POST'
-            , self.url + "/api/actions"
-            , headers=qheader
-            , data=templ
+        qheader = {"Content-Type": "application/x-www-form-urlencoded"}
+
+        req = requests.Request(
+            "POST", self.url + "/api/actions", headers=qheader, data=templ
         )
 
         prepped = self.sess.prepare_request(req)
-            
-        result = self.sess.send(prepped, verify = False)
 
-        if (result.status_code == 200):
+        result = self.sess.send(prepped, verify=False)
+
+        if result.status_code == 200:
             print(result)
             return bigfixActionResult(result.content)
         else:
             return None
-
